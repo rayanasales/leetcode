e875e01acecc5e0024a11207f0c293b67d7a3cc0
rayanasales <rayanaasales@gmail.com>
2024-08-30 23:33:18 -0300

    2619. Array Prototype Last

--- 2619. Array Prototype Last/2619.ArrayPrototypeLast.js
+++ 2619. Array Prototype Last/2619.ArrayPrototypeLast.js
@@ -1,15 +1,3 @@
-# 2619. Array Prototype Last
-
-[🍄 2619. Array Prototype Last](https://leetcode.com/problems/array-prototype-last/description/)
-
-<aside>
-⏰ Solved in 05:19 mins
-</aside>
-</br>
-
-# Solution
-
-```js
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
@@ -26,4 +14,3 @@ Array.prototype.last = function () {
  * const arr = [1, 2, 3];
  * arr.last(); // 3
  */
-```
