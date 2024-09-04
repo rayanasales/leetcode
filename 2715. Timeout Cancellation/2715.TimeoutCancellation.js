445e00e75fbdc5363591f249250d012c0bf0e2fc
rayanasales <rayanaasales@gmail.com>
2024-09-04 14:42:02 -0300

    2715. Timeout Cancellation

---
+++ 2715. Timeout Cancellation/2715.TimeoutCancellation.js
@@ -0,0 +1,13 @@
+var cancellable = function (fn, args, t) {
+  let timeoutId;
+
+  const cancelFn = () => {
+    clearTimeout(timeoutId);
+  };
+
+  timeoutId = setTimeout(() => {
+    fn(...args);
+  }, t);
+
+  return cancelFn;
+};
